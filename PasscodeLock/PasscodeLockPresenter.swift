c2c8fb5b3f5f1da07e0a1532dcaf361c713f8c9d
Sathya Rajaraman <trsathya@gmail.com>
2017-05-20 09:49:03 +0530

    Fix a bug.

--- PasscodeLock/PasscodeLockPresenter.swift
+++ PasscodeLock/PasscodeLockPresenter.swift
@@ -102,6 +102,7 @@ open class PasscodeLockPresenter {
 
 extension PasscodeLockPresenter: PasscodeLockViewDelegate {
     public func success() {
+        dismissPasscodeLock()
         viewDelegate?.success()
     }
     
